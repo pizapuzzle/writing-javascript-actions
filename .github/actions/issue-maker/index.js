494d2a07e53daa6cdb324659009dd793fbc841e5
ppris <49683462+pPris@users.noreply.github.com>
2021-07-28 18:59:23 +0800

    create index.js

---
+++ .github/actions/issue-maker/index.js
@@ -0,0 +1,25 @@
+const core = require("@action/core");
+const github = require("@action/github");
+
+async function run() {
+    try {
+        const issueTitle = core.getInput("issueTitle");
+        const jokeBody = core.getInput("joke");
+        const token = core.getInput("repo-token");
+
+        const octokit = github.getOctokit(token); // ! note
+
+        // how did they get issues.create? ans:
+        // https://octokit.github.io/rest.js/v18#issues
+        const newIssue = await octokit.issues.create({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo, // !where is this from ?
+            title: issueTitle,
+            body: jokeBody,
+        });
+    } catch (err) {
+        core.setFailed(err.message);
+    }
+}
+
+run();
